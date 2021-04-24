7444faff419f36ec5c4ad3bd5b615eb18c0136f4
enternen <83123955+enternen@users.noreply.github.com>
2021-04-24 16:24:39 +0300

    Create pingpong.py
    создан класс GameSprite и от него унаследован класс Player.

---
+++ pingpong.py
@@ -0,0 +1,28 @@
+from pygame import *
+class GameSprite(sprite.Sprite):
+    def __init__(self, p_image,p_x,p_y,size_x, size_y, p_speed):
+        sprite.Sprite.__init__(self)
+
+        self.image = transform.scale(image.load(p_image), (size_x, size_y))
+        self.speed = p_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = p_x
+        self.rect.y = p_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.x > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.x < 520:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.x > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.x < 520:
+            self.rect.y += self.speed
