a18d99e21e4b1737e5a09979d70dead1cd0b34ed
enternen <83123955+enternen@users.noreply.github.com>
2021-04-25 11:48:59 +0300

    Update pingpong.py
    исправлен баг с надписями

--- pingpong.py
+++ pingpong.py
@@ -26,6 +26,14 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < 455:
             self.rect.y += self.speed
+
+
+font.init()
+font = font.Font(None,35)
+lose1 = font.render("PLAYER 1 LOSE!",True,(180,0,0))
+lose2 = font.render("PLAYER 2 LOSE!",True,(180,0,0))
+
+
 window = display.set_mode((600,500))
 window.fill((200,255,255))
 win_height = 500
@@ -35,15 +43,13 @@ clock = time.Clock()
 
 speed_x = 3
 speed_y = 3
-# создаем спрайты для игры
-racket1 = Player("",30,200,4,50,10)
-racket2 = Player("",520,200,4,50,10)
-ball = Player("",200,200,25,25,25)
-# создаем надписи
-font.init()
-font = font.Font(None,35)
-lose1 = font.render("PLAYER 1 LOSE!",True,(180,0,0))
-lose2 = font.render("PLAYER 2 LOSE!",True,(180,0,0))
+#создаем спрайты для игры
+racket1 = Player("racket.png",30,200,4,50,10)
+racket2 = Player("racket.png",520,200,4,50,10)
+ball = Player("ball.png",200,200,25,25,25)
+#создаем надписи
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -55,6 +61,7 @@ while game:
         racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+        
         if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
             speed_x *= -1
             speed_y *= 1
@@ -64,9 +71,10 @@ while game:
 
         if ball.rect.x < 0:
             finish = True
+            
             window.blit(lose1, (200,200))
             game_over = True
-        if ball.rect.x < win_width:
+        if ball.rect.x > win_width:
             finish = True
             window.blit(lose2, (200,200))
             game_over = True
@@ -74,7 +82,5 @@ while game:
     racket2.reset()
     
     ball.reset()
-        
-
     display.update()
     clock.tick(60)
